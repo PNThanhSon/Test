d35df660523d03c86ab79f5108909631a6c50e64
SON <23521360@gm.uit.edu.vn>
2024-09-21 20:42:59 +0700

    Test

---
+++ SQLQueryBT2.sql
@@ -0,0 +1,39 @@
+﻿--Query của 23521360 Phạm Nguyễn Thanh Sơn
+--Sinh viên hoàn thành Phần I bài tập QuanLyBanHang từ câu 2 đến câu 10
+
+--2. Thêm vào thuộc tính GHICHU có kiểu dữ liệu varchar(20) cho quan hệ SANPHAM.
+ALTER TABLE SANPHAM ADD GHICHU VARCHAR(20);
+
+--3. Thêm vào thuộc tính LOAIKH có kiểu dữ liệu là tinyint cho quan hệ KHACHHANG
+ALTER TABLE KHACHHANG ADD LOAIKH TINYINT;
+
+--4. Sửa kiểu dữ liệu của thuộc tính GHICHU trong quan hệ SANPHAM thành varchar(100)
+ALTER TABLE SANPHAM
+ALTER COLUMN GHICHU VARCHAR(100);
+
+--5. Xóa thuộc tính GHICHU trong quan hệ SANPHAM
+ALTER TABLE SANPHAM DROP COLUMN GHICHU;
+
+/*6. Làm thế nào để thuộc tính LOAIKH trong quan hệ KHACHHANG
+có thể lưu các giá trị là: “Vang lai”, “Thuong xuyen”, “Vip”…
+
+Em sử dụng kiểu dữ liệu varchar(20)
+*/
+ALTER TABLE KHACHHANG
+ALTER COLUMN LOAIKH VARCHAR(20);
+
+--7. Đơn vị tính của sản phẩm chỉ có thể là (“cay”,”hop”,”cai”,”quyen”,”chuc”)
+ALTER TABLE SANPHAM
+ADD CONSTRAINT CHK_DVT CHECK (DVT IN ('cay', 'hop', 'cai', 'quyen', 'chuc'));
+
+--8. Giá bán của sản phẩm từ 500 đồng trở lên.
+ALTER TABLE SANPHAM
+ADD CONSTRAINT CHK_GIA CHECK (GIA >= 500);
+
+--9. Mỗi lần mua hàng, khách hàng phải mua ít nhất 1 sản phẩm.
+ALTER TABLE CTHD
+ADD CONSTRAINT CHK_SL CHECK (SL >= 1);
+
+--10. Ngày khách hàng đăng ký là khách hàng thành viên phải lớn hơn ngày sinh của người đó
+ALTER TABLE KHACHHANG
+ADD CONSTRAINT CHK_NGDK CHECK (NGDK > NGSINH);
---
+++ SQLQueryBT3.sql
@@ -0,0 +1,33 @@
+﻿--Query của 23521360 Phạm Nguyễn Thanh Sơn
+--Sinh viên hoàn thành Phần I bài tập QuanLyGiaoVu từ câu 3 đến câu 8
+
+--3. Thuộc tính GIOITINH chỉ có giá trị là “Nam” hoặc “Nu”.
+ALTER TABLE GIAOVIEN
+ADD CONSTRAINT CHK_GIOITINHGV CHECK (GIOITINH IN ('Nam', 'Nu'))
+ALTER TABLE HOCVIEN
+ADD CONSTRAINT CHK_GIOITINHHV CHECK (GIOITINH IN ('Nam', 'Nu'));
+
+--4. Điểm số của một lần thi có giá trị từ 0 đến 10 và cần lưu đến 2 số lẽ (VD: 6.22).
+ALTER TABLE KETQUATHI
+ALTER COLUMN DIEM DECIMAL(4, 2); -- Đổi thành kiểu dữ liệu lưu đến 2 thập phân lẻ trước
+ALTER TABLE KETQUATHI
+ADD CONSTRAINT CHK_DIEM CHECK (DIEM >= 0 AND DIEM <= 10); --Ràng buộc điểm từ 0 đến 10 sau
+
+--5. Kết quả thi là “Dat” nếu điểm từ 5 đến 10 và “Khong dat” nếu điểm nhỏ hơn 5.
+ALTER TABLE KETQUATHI ADD CONSTRAINT CHK_KETQUA CHECK
+(	
+	(KQUA = 'Dat' AND DIEM >= 0 AND DIEM <= 10)
+	OR (KQUA = 'Khong dat' AND DIEM < 5)
+)
+
+--6. Học viên thi một môn tối đa 3 lần
+ALTER TABLE KETQUATHI
+ADD CONSTRAINT CHK_LANTHI CHECK(LANTHI <= 3);
+
+--7. Học kỳ chỉ có giá trị từ 1 đến 3.
+ALTER TABLE GIANGDAY
+ADD CONSTRAINT CHK_HOCKY CHECK (HOCKY >= 1 AND HOCKY <= 3);
+
+--8. Học vị của giáo viên chỉ có thể là “CN”, “KS”, “Ths”, ”TS”, ”PTS”.
+ALTER TABLE GIAOVIEN
+ADD CONSTRAINT CHK_HOCVI CHECK (HOCVI IN ('CN', 'KS' , 'Ths' , 'TS', 'PTS'));
